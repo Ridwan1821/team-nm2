d158e0bdd3b5a96522529c910e36d01f741b6df6
kolawole ridwan <kolawoleridwan416@gmail.com>
2022-12-14 11:51:16 -1100

    streamlit additional page

---
+++ pages/2_eda.py
@@ -0,0 +1,56 @@
+# Streamlit dependencies
+import streamlit as st
+
+# Data dependencies
+import numpy as np
+import pandas as pd
+import PIL
+from PIL import Image
+
+def main():
+        """Tweet Classifier App with Streamlit """
+        img = Image.open('conroy.png')
+        st.image(img, caption=None)
+        st.subheader("Exploratory Data Analysis - EDA")
+        st.markdown("The data that was used to train our models is a tweet data that contain 15819 rows and 3 columns. The columns are sentiment, message and tweetid while the rows contain records having sentiment, message and tweetid.  \nAfter exploring this data, the following insights were uncovered:")
+        img18 = Image.open('eda1.jpeg')
+        img19 = img18.resize((200,200))
+        img20 = Image.open('eda2.jpeg')
+        img21 = img20.resize((200,200))
+        img22 = Image.open('eda3.jpeg')
+        img23 = img22.resize((200,200))
+        img24 = Image.open('eda4.jpeg')
+        img25 = img24.resize((200,200))
+        col1, col2, col3 = st.columns(3)
+        with col1:
+                st.image(img19, caption="First five records", use_column_width=False)
+        with col2:
+                st.image(img21, caption="Sentiments", use_column_width=False)
+        with col3:
+                st.write("53.9% of the observations are pro climate change.  \n8.2% are anti climate change.  \n14.9% are neutral while 23% of the observations fall into the NEWS category.")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+                st.image(img23, caption="Imbalanced data", use_column_width=False)
+        with col2:
+                st.image(img25, caption="Balanced data", use_column_width=False)
+        with col3:
+                st.write("Our data is imbalanced as some sentiments have way more observations than others.  \nThe data was balanced so that every sentiment category will have equal number of observations. This will help remove some bias from the model")
+        st.markdown("The message column is the predictive feature of the data. As such, we removed stop words, urls and punctuations from the column. We then applied tokenization and lemmatization before we did data preprocessing.  \n The following images contain word clouds gotten from messages under each category of sentiments.")
+        img26 = Image.open('pro.jpeg')
+        img27 = img26.resize((200,200))
+        img28 = Image.open('anti.jpeg')
+        img29 = img29.resize((200,200))
+        img30 = Image.open('neutral.jpeg')
+        img31 = img30.resize((200,200))
+        col1, col2, col3 = st.columns(3)
+        with col1:
+                st.image(img27, caption="Pro climate change", use_column_width=False)
+        with col2:
+                st.image(img29, caption="Anti climate change", use_column_width=False)
+        with col3:
+                st.write(img31, caption="Neutral", use_column_width=False)
+
+        #st.subheader("How to use the app")
+        #st.markdown("Select a model and then enter a text that is about climate change on the text box. \nOnce you are done, click on Classify to apply the model on the text. \nThe sentiment of the text will be stated.")
+if __name__ == '__main__':
+	main()
